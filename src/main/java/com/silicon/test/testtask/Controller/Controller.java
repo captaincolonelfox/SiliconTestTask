58485e684caf8972dabed6d504747f1144ab3aa6
iafst <lentrog@gmail.com>
2018-05-28 17:02:25 +0300

    make edit work

--- src/main/java/com/silicon/test/testtask/Controller/Controller.java
+++ src/main/java/com/silicon/test/testtask/Controller/Controller.java
@@ -2,16 +2,13 @@ package com.silicon.test.testtask.Controller;
 
 import com.silicon.test.testtask.Model.Item;
 import com.silicon.test.testtask.Model.Category;
-import com.silicon.test.testtask.Model.ItemId;
 import com.silicon.test.testtask.Repo.IItemsRepository;
 import com.silicon.test.testtask.Service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.invoke.MethodType;
 import java.util.List;
 
 @org.springframework.stereotype.Controller
@@ -25,23 +22,34 @@ public class Controller {
     @RequestMapping("/viewCategories")
     public String showCategories(Model model) {
         model.addAttribute(categoryService.getAllCategories());
-        model.addAttribute("newCat", "false");
+        model.addAttribute("newCat", false);
         return "viewCategories";
     }
 
     @GetMapping(value = "/viewCategories", params = "newCat")
     public String newCategory(Model model) {
-        model.addAttribute("newCat", "true");
+        model.addAttribute("newCat", true);
         model.addAttribute(categoryService.getAllCategories());
         model.addAttribute("category", new Category());
         return "viewCategories";
     }
 
-    @PostMapping(value = "/viewCategories", params = {"add"})
-    public String addCategory(@ModelAttribute Category category, BindingResult bindingResult) {
+    @GetMapping(value = "/viewCategories", params = {"edit"})
+    public String editCategory(Model model, @RequestParam("edit") String editCategory) {
+        model.addAttribute("categoryToEdit", editCategory);
+        model.addAttribute(categoryService.getAllCategories());
+        model.addAttribute("category", new Category());
+        return "viewCategories";
+    }
+
+    @PostMapping(value = "/viewCategories")
+    public String addCategory(@ModelAttribute Category category, BindingResult bindingResult,
+                              @RequestParam(value = "edit", required = false, defaultValue = "") String categoryToEdit) {
         if (bindingResult.hasErrors()) {
             return "viewCategories";
         }
+        if (categoryToEdit != null && !categoryToEdit.equals(""))
+            category.setName(categoryToEdit);
         this.categoryService.addCategory(category);
         return  "redirect:/viewCategories";
     }
