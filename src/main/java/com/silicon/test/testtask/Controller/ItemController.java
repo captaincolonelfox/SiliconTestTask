51437f05ad523273426cffab92f024eb9a8f4d1f
iafst <lentrog@gmail.com>
2018-05-29 23:05:43 +0300

    add url encoder for deleting parameter

--- src/main/java/com/silicon/test/testtask/Controller/ItemController.java
+++ src/main/java/com/silicon/test/testtask/Controller/ItemController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.util.List;
 
 @Controller
@@ -36,15 +39,15 @@ public class ItemController {
     }
 
     @PostMapping(value = "/editItems")
-    public String updateItems(@ModelAttribute Item item) {
+    public String updateItems(@ModelAttribute Item item) throws UnsupportedEncodingException {
         itemRepo.save(item);
-        return "redirect:/viewItems?category=" + item.getCategory();
+        return "redirect:/viewItems?category=" + URLEncoder.encode(item.getCategory(), "UTF-8");
     }
 
-    @GetMapping(value = "/viewItems", params = {"delete", "cat", "name"})
+    @GetMapping(value = "/editItems", params = {"delete", "cat", "name"})
     public String deleteItems(@RequestParam("cat") String catName,
-                              @RequestParam("name") String itemName) {
+                              @RequestParam("name") String itemName) throws UnsupportedEncodingException {
         itemRepo.delete(itemRepo.findByItemId(new ItemId(catName, itemName)));
-        return "redirect:/viewItems?category=" + catName;
+        return "redirect:/viewItems?category=" + URLEncoder.encode(catName, "UTF-8");
     }
 }
